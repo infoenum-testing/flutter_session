b9b9790c0b4827fa77ddd19a691d54a2c44d3378
infoenum <ideviftekhar@gmail.com>
2023-12-02 17:56:22 +0530

    working on onbording screen

--- lib/screens/onbording_screen.dart
+++ lib/screens/onbording_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_demo/models/onbording_data_model.dart';
+import 'package:flutter_application_demo/screens/home_screen.dart';
 
 class OnbordingScreen extends StatefulWidget {
   OnbordingScreen({super.key});
@@ -30,13 +31,28 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
             "Some of the most popular apps using Flutter are Reflectly, Google Ads, Alibaba, Tencent, Birch Finance, Coach Yourself, and Watermaniac. In general, Flutter is used to develop android, iOS, Linux, Mac, and Web applications from a single codebase or with high brand designs.")
   ];
 
-  StreamController<int> _streamController = StreamController<int>();
-
+  StreamController<int> _streamController = StreamController<int>.broadcast();
+  PageController _pageController = PageController();
   @override
   void initState() {
     // TODO: implement initState
     _streamController.sink.add(0);
     super.initState();
+  }
+
+  get pageisLast {
+    return _pageController.page == onbordingData.length - 1;
+  }
+
+  forwordAction() {
+    if (pageisLast) {
+      Navigator.push(context, MaterialPageRoute(builder: ((context) {
+        return HomeScreen();
+      })));
+    } else {
+      _pageController.nextPage(
+          duration: Duration(milliseconds: 200), curve: Curves.ease);
+    }
   }
 
   @override
@@ -45,11 +61,13 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
       body: Stack(
         children: [
           PageView.builder(
+              controller: _pageController,
               itemCount: onbordingData.length,
               onPageChanged: (value) {
                 _streamController.sink.add(value);
               },
               itemBuilder: (context, index) {
+                // debugPrint("======${_pageController.page}");
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -92,6 +110,25 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
                               width: 20,
                               height: 20,
                             )),
+                  ),
+                );
+              }),
+          StreamBuilder<int>(
+              stream: _streamController.stream,
+              builder: (context, snapshot) {
+                return Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: FloatingActionButton(
+                    backgroundColor: snapshot.data == onbordingData.length - 1
+                        ? Colors.brown
+                        : Colors.black,
+                    onPressed: () {
+                      forwordAction();
+                    },
+                    child: Text(snapshot.data == onbordingData.length - 1
+                        ? "Start"
+                        : "Next"),
                   ),
                 );
               })
